0c0d7ac6d8e12dc3c0132f35f1984c7963b8e902
ArkharovDanil <maslinaraz@gmail.com>
2022-04-24 13:20:20 +0300

    First 2 tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,20 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+#include "textgen.h"
+
+TEST(task1, test1) {
+    TextGen tg = TextGen("Text2.txt", 2);
+    std::string text = tg.CreateText(1000);
+    TextGen::prefix temp = tg.GetPref(0);
+    ASSERT_EQ(temp.size(), 2);
+}
+
+TEST(task2, test2) {
+    Textgen tg = TextGen("Text3.txt", 2);
+    std::string text = tg.CreateText(1000);
+    TextGen::prefix temp = tg.GetPref(0);
+    std::vector<std::string> temporarySuffix = tg.ChooseSuffix(temp);
+    ASSERT_EQ(temporarySuffix[0], "family");
+}
